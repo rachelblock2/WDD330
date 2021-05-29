1776c7951f520f6dbc9df78cb25967911b467001
rachelblock2 <lof16002@byui.edu>
2021-05-29 15:50:03 -0600

    Removed extra code, added comments

--- week5/scripts/todos.js
+++ week5/scripts/todos.js
@@ -2,32 +2,32 @@ import {
     Todo
 } from './todo.js';
 
-let toDoList = [];
+let toDoList = []; //The list that holds the task objects
 
 let addNewTask = document.querySelector('#addNewTask');
 let taskInput = document.querySelector('#task_input')
 
-
+// Load saved tasks into the webpage if there are any that exist
 if (localStorage.getItem("Tasks")) {
     toDoList = JSON.parse(localStorage.getItem("Tasks"));
     displayTasks(toDoList);
 }
 
+// When user enters a string into the input, add it to the list of tasks
 addNewTask.addEventListener('touchend', (e) => {
     if (taskInput.value !== '' || null) {
         let newTodo = new Todo(taskInput.value);
         toDoList.push(newTodo);
         localStorage.setItem(`Tasks`, JSON.stringify(toDoList));
-        taskInput.value = ''
+        taskInput.value = '' //Clear the input field
         displayTasks(toDoList)
     }
 })
 
-
+// Show the list of tasks on the webpage
 function displayTasks(toDoList) {
     let ul = document.querySelector('ul');
     ul.innerHTML = ''
-    console.log(ul)
 
     toDoList.forEach(
         todoItem => {
@@ -36,20 +36,17 @@ function displayTasks(toDoList) {
                                 <span class=${todoItem.Completed ? "strikethrough" : ""}>${todoItem.Content}</span>
                                 <input type="image" src="images/trash_png.png" data-id="${todoItem.Id}" name="${todoItem.Id}">
                             </li>`;
-            console.log(ul.innerHTML)
         }
     );
-    console.log(ul.innerHTML)
-    console.log(ul)
 
     checkBox();
     addDeleteBtns();
     createAllTaskBtn();
     createActiveBtn();
-    createCompletedBtn(); //BUG HERE
+    createCompletedBtn();
 };
 
-
+// Add functionality to delete buttons created with the tasks
 function addDeleteBtns() {
     let deleteTasks = document.querySelectorAll('input[type="image"]');
 
@@ -57,10 +54,9 @@ function addDeleteBtns() {
         task => {
             task.addEventListener('click', (e) => {
                 let selectedId = e.target.dataset.id; //Select trash image id that was clicked
-                let selectedTask = toDoList.findIndex(todo => todo.Id === parseInt(selectedId)); //Find the index of the selected id in the list
-                toDoList.splice(selectedTask, 1); 
+                let selectedTask = toDoList.findIndex(todo => todo.Id === parseInt(selectedId)); //Find the index of the selected id in the list of task objects
+                toDoList.splice(selectedTask, 1); //Remove that object
 
-                console.log(toDoList)
                 displayTasks(toDoList);
                 localStorage.setItem('Tasks', JSON.stringify(toDoList));
             });
@@ -68,26 +64,26 @@ function addDeleteBtns() {
     );
 }
 
-
+// Add functionality to checkbox buttons created with the tasks
 function checkBox() {
     let checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     checkboxes.forEach(
         checkbox => {
             checkbox.addEventListener('click', (e) => {
-                let selectedId = e.target.dataset.id;
+                let selectedId = e.target.dataset.id; //Select checkbox id that was clicked
                 let selectedTask = toDoList.find(todo => todo.Id === parseInt(selectedId));
-                selectedTask.Completed = !selectedTask.Completed;
+                selectedTask.Completed = !selectedTask.Completed; //Change task to completed status 
 
                 let checkedIndex = toDoList.findIndex(todo => todo.Id === parseInt(selectedId));
-                crossOutTask(checkedIndex);
+                crossOutTask(checkedIndex); //Cross out completed task
                 localStorage.setItem(`Tasks`, JSON.stringify(toDoList));
             });
         }
     );
 };
 
-
+// Cross out completed task
 function crossOutTask(checkboxIndex) {
     let taskNames = document.querySelectorAll('span');
 
@@ -97,6 +93,7 @@ function crossOutTask(checkboxIndex) {
     localStorage.setItem(`Tasks`, JSON.stringify(toDoList));
 }
 
+// Show all tasks
 function createAllTaskBtn () {
     let allTaskBtn = document.querySelector('#all_tasks')
     allTaskBtn.addEventListener('click', (e) => {
@@ -104,6 +101,7 @@ function createAllTaskBtn () {
     });
 };
 
+// Show all uncompleted tasks
 function createActiveBtn () {
     let activeBtn = document.querySelector('#active_tasks')
     activeBtn.addEventListener('click', (e) => {
@@ -112,10 +110,10 @@ function createActiveBtn () {
     });
 };
 
+// Show all completed tasks
 function createCompletedBtn () {
     let completeBtn = document.querySelector('#complete_tasks')
     completeBtn.addEventListener('click', (e) => {
-        console.log('clicked complete')
         let filter = toDoList.filter(todo => todo.Completed === true)
         displayTasks(filter);
     });
