ad17c870f2cbf2fb24d7f9392111763ccc06f804
rachelblock2 <lof16002@byui.edu>
2021-07-17 22:31:27 -0600

    Updated links to notes and team assignments

--- week1/scripts/navLinks.js
+++ week1/scripts/navLinks.js
@@ -16,8 +16,29 @@ const links = [
     url: "../week4/index.html"
     },
     {
-    label: "Week5 notes",
+    label: "Week5-6 notes",
     url: "../week5/index.html"
+    },
+    // Week 5, 6 and 7 may have some overlap which is why week 6 notes are technically missing, however I did take notes on it
+    {
+    label: "Week6-7 notes",
+    url: "../week7/index.html"
+    },
+    {
+    label: "Week8 notes",
+    url: "../week8/index.html"
+    },
+    {
+    label: "Week9 notes",
+    url: "../week9/index.html"
+    },
+    {
+    label: "Week10 notes",
+    url: "../week10/index.html"
+    },
+    {
+    label: "Week11 notes",
+    url: "../week11/index.html"
     },
     {
     label: "Week2 Team Assignment",
@@ -32,8 +53,32 @@ const links = [
     url: "../week5/hiking-start.html"
     },
     {
+    label: "Week7 Team Assignment",
+    url: "../week7/hikes5/hiking-complete.html"
+    },
+    {
+    label: "Week8 Team Assignment",
+    url: "../week8/team_activity.html"
+    },
+    {
+    label: "Week9 Team Assignment",
+    url: "../week9/index.html"
+    },
+    {
+    label: "Week10 Team Assignment",
+    url: "../week10/teamAssignment.html"
+    },
+    {
+    label: "Week11 Team Assignment",
+    url: "../week11/auth/client/week11.html"
+    },
+    {
     label: "To Do App Project",
     url: "../week6/todo.html"
+    },
+    {
+    label: "Recipes Final App Project",
+    url: "../final_project/recipesHome.html"
     }
 ]
 
